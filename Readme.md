75d24fb3fb4eb8dcbee4a75219dde54980515a65
lanut <997622499@qq.com>
2024-06-04 16:39:10 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# 基于微信点餐系统的设计与实现
+
+后端模块：
+
+[ordering_backend](ordering_backend)
+
+前段模块：
+
+待定
--- ordering_backend/src/main/java/com/lanut/ordering_backend/web/TestController.kt
+++ ordering_backend/src/main/java/com/lanut/ordering_backend/web/TestController.kt
@@ -1,4 +1,15 @@
 package com.lanut.ordering_backend.web
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+
+@Controller
 class TestController {
+    @ResponseBody
+    @GetMapping("/")
+    fun index(): String {
+        return "Hello World"
+    }
 }
