285fbb14fe95cffd565b887801f5c55c89769014
lanut <997622499@qq.com>
2024-06-10 22:20:00 +0800

    配置springSecurity基本内容

---
+++ ordering_backend/src/main/java/com/lanut/ordering_backend/entity/RestBean.kt
@@ -0,0 +1,37 @@
+package com.lanut.ordering_backend.entity
+
+import com.alibaba.fastjson2.JSONWriter
+import com.alibaba.fastjson2.toJSONString
+
+data class RestBean<T>(val code: Int, val data: T?, val message: String) {
+    companion object {
+        @JvmStatic
+        fun <T> success(data: T): RestBean<T> {
+            return RestBean(200, data, "请求成功")
+        }
+
+        @JvmStatic
+        fun <T> failure(code: Int, message: String): RestBean<T> {
+            return RestBean(code, null, message)
+        }
+
+        @JvmStatic
+        fun <T> failure(code: Int): RestBean<T> {
+            return failure(code, "请求失败")
+        }
+    }
+
+    fun asJsonString(): String {
+        return this.toJSONString(JSONWriter.Feature.WriteNulls)
+    }
+}
+
+@Suppress("FunctionName")
+fun <T> T.RestSuccess(): RestBean<T> {
+    return RestBean.success(this)
+}
+
+@Suppress("FunctionName")
+fun String.RestFailure(code: Int): RestBean<String> {
+    return RestBean.failure(code, this)
+}
