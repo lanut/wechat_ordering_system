d2ee380a30814586c0a92ad89707f96048cc9581
lanut <997622499@qq.com>
2024-06-12 23:35:52 +0800

    添加jwt令牌验证通过的情景

--- ordering_backend/src/main/java/com/lanut/ordering_backend/configuration/SecurityConfiguration.kt
+++ ordering_backend/src/main/java/com/lanut/ordering_backend/configuration/SecurityConfiguration.kt
@@ -7,20 +7,13 @@ import com.lanut.ordering_backend.entity.vo.AuthorizeVO
 import com.lanut.ordering_backend.filter.JwtAuthorizeFilter
 import com.lanut.ordering_backend.utils.JwtUtils
 import jakarta.annotation.Resource
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.AuthenticationFailureHandler
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 
 @Configuration
 open class SecurityConfiguration {
